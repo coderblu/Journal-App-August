a153aee50fee3b0821a82d8002bb5c86ef340ed5
Akash Sahu <arysontesters@gmail.com>
2025-03-08 09:55:35 +0530

    from officee

--- src/main/java/net/engineeringdigest/journalApp/Repository/ConfigJournalAppRepository.java
+++ src/main/java/net/engineeringdigest/journalApp/Repository/ConfigJournalAppRepository.java
@@ -1,8 +1,8 @@
 package net.engineeringdigest.journalApp.Repository;
 
-import com.mongodb.client.MongoDatabase;
 import net.engineeringdigest.journalApp.Entity.ConfigJournalAppEntity;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface configJournalAppRepository extends MongoRepository<ConfigJournalAppEntity,Id> {
+public interface ConfigJournalAppRepository extends MongoRepository<ConfigJournalAppEntity, ObjectId> {
 }
--- src/main/java/net/engineeringdigest/journalApp/cache/AppCache.java
+++ src/main/java/net/engineeringdigest/journalApp/cache/AppCache.java
@@ -1,5 +1,8 @@
 package net.engineeringdigest.journalApp.cache;
 
+import net.engineeringdigest.journalApp.Entity.ConfigJournalAppEntity;
+import net.engineeringdigest.journalApp.Repository.ConfigJournalAppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,10 +11,16 @@ import java.util.Map;
 
 @Component
 public class AppCache {
+    @Autowired
+    ConfigJournalAppRepository configJournalAppRepository;
     private Map<String, String> appCache;
     @PostConstruct
     public void init()
     {
-        List<ConfigJournalAppEntity> all = confi
+        List<ConfigJournalAppEntity> all = configJournalAppRepository.findAll();
+        for (ConfigJournalAppEntity entry:all)
+        {
+
+        }
     }
 }
