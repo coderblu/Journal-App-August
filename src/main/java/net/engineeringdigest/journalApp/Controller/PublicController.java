a9910ae22f63a994e4b807d900376f6325b78553
Akash Sahu <arysontesters@gmail.com>
2025-03-11 16:39:00 +0530

    Complete JWT Authentication and Authorisation in Spring Boot

--- src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
+++ src/main/java/net/engineeringdigest/journalApp/Controller/PublicController.java
@@ -1,17 +1,26 @@
 package net.engineeringdigest.journalApp.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.engineeringdigest.journalApp.Entity.User;
+import net.engineeringdigest.journalApp.Service.UserDetailsServiceImpl;
 import net.engineeringdigest.journalApp.Service.UserService;
+import net.engineeringdigest.journalApp.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
 
@@ -19,20 +28,42 @@ public class PublicController {
     private String apiKey;
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
     @GetMapping("/health-check")
     public String healthCheck()
     {
         return apiKey;
     }
-    @PostMapping("/create-user")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    @PostMapping("/signup")
+    public ResponseEntity<User> signup(@RequestBody User user) {
             try {
                 userService.saveNewUser(user);
                 return new ResponseEntity<User>(user, HttpStatus.CREATED);
             } catch (Exception e) {
+                log.error("Exception occured whuke create");
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
     }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+        return new ResponseEntity<>(jwt,HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @GetMapping("/get-all")
     public ResponseEntity<List<User>> getAllUsers()
     {
